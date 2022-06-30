d251b82101af4c11807e26afab9a324abaf41973
Sumanth Perambuduri <sumanthpera3@gmail.com>
2022-06-30 19:56:32 +0530

    home section and formatting

--- src/script.js
+++ src/script.js
@@ -1,57 +1,57 @@
 const phrases = [
-    "GITAM Visakhaptnam", "GITAM Hyderabad", "GITAM Bengaluru", "Best Gitam tech community"
+  "GITAM Visakhapatnam", "GITAM Hyderabad", "GITAM Bengaluru", "Best Gitam tech community", "Most active Gitam tech club"
 ]
 
 animateText()
 
 function animateText() {
-    const elem = document.getElementById('mytext')
-    elem.classList.add('animate-typed-out')
-    setInterval(() => {
-        elem.innerHTML = phrases[getRndInteger(0, phrases.length-1)]
-    }, 5000);
+  const elem = document.getElementById('mytext')
+  elem.classList.add('animate-typed-out')
+  setInterval(() => {
+    elem.innerHTML = phrases[getRndInteger(0, phrases.length - 1)]
+  }, 5000);
 }
 console.log('sjchdb')
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
-$(document).ready(function(){
-    $('.items').slick({
-  dots: true,
-  infinite: true,
-  speed: 800,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+$(document).ready(function () {
+  $('.items').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
 
-  ]
+    ]
+  });
 });
-          });
