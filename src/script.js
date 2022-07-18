c025030001265576a29dae8e2a9a5a4d559dcf74
Sumanth Perambuduri <sumanthpera3@gmail.com>
2022-07-18 18:11:06 +0530

    new design, better performance

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+document.addEventListener('scroll', () => {
+  document.getElementById('body').setAttribute('data-scroll', window.scrollY)
+});
+
 const phrases = [
   "GITAM Visakhapatnam", "GITAM Hyderabad", "GITAM Bengaluru", "Best Gitam tech community", "Most active Gitam tech club"
 ]
@@ -11,47 +15,8 @@ function animateText() {
     elem.innerHTML = phrases[getRndInteger(0, phrases.length - 1)]
   }, 5000);
 }
-console.log('sjchdb')
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-$(document).ready(function () {
-  $('.items').slick({
-    dots: true,
-    infinite: true,
-    speed: 800,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-
-    ]
-  });
-});
